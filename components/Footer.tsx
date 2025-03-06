d19b94049aa285bbbabb53f0852d19548fb74102
Morris Wong <morris.wong@wtwco.com>
2025-03-06 17:34:35 +0000

    Update footer branding and link to new website

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,14 @@ export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       <div>
-        Powered by{' '}
+        Built by{' '}
         <a
-          href="https://togetherai.link"
+          href="https://bento.me/wch"
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
         >
-          Meta Llama
+          Morris Wong 
         </a>
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
